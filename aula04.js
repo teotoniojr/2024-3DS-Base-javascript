af5e4c0d507051589becb968f174bc1d23198acc
Teotonio José de Souza Junior <102707827+teotoniojr@users.noreply.github.com>
2024-08-22 12:07:38 -0300

    Add files via upload

--- aula04.js
+++ aula04.js
@@ -47,3 +47,29 @@ function multiplicacao(numero4 = 2, numero5 = 5){
 }
 
 imprimeTexto(multiplicacao(somaDeNumeros(3,4), somaDeNumeros(5,10))); 
+
+// expressão de função
+
+const constanteSoma = function(nume1, nume2){return nume1 + nume2}
+
+console.log(constanteSoma(3,6))
+
+// Funções e var são "listadas" no topo do arquivo
+
+// Arrow function
+
+const apresentaArrow = nome => `meu nome é ${nome}`;
+const somaEmFlecha = (nu1, nu2) => nu1 + nu2;
+
+console.log(apresentaArrow("Teo"))
+console.log(somaEmFlecha(2,4))
+
+const somaNumerosPequenos = (num1, num2) => {
+    if(num1 > 10 || num2 > 10){
+        return "Essa função é apenas para números pequenos"
+    } else {
+        return num1 + num2;
+    }
+}
+
+console.log(somaNumerosPequenos(2,3))
